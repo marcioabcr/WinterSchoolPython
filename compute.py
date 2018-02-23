6c489172fda386aae2a80ece4b97bec0f24adbcd
marcioabcr <32570044+marcioabcr@users.noreply.github.com>
2018-02-23 11:49:30 +0100

    Division

--- compute.py
+++ compute.py
@@ -1,7 +1,12 @@
 from __future__ import division
+import numpy as np
 
 def divide(a,b):
-    return a / b
+    try:
+        x = a / b
+    except ZeroDivisionError
+        x = np.inf
+    return x
 
 def multiply(a,b):
     return a * b
