76260d7eb72794aab357cd1e26ccb142762118e8
marcioabcr <32570044+marcioabcr@users.noreply.github.com>
2018-02-23 11:38:23 +0100

    Correct test

--- test_compute.py
+++ test_compute.py
@@ -2,11 +2,9 @@ import pytest
 from compute import divide
 from compute import multiply
 
-@pytest.mark.parametrize('a,b,x',[(1,2,0.5),(4,2,2)])
-
 def test_divide():
-    x = divide(a,b)
-    assert x == pytest.approx(x)
+    x = divide(1,2)
+    assert x == pytest.approx(0.5)
 
 def test_multiply():
     assert multiply(2,2) == 4
