e59d4f79cf6b076dc79b89cbb7cd68b07786c966
marcioabcr <32570044+marcioabcr@users.noreply.github.com>
2018-02-23 12:03:58 +0100

    Test

--- test_compute.py
+++ test_compute.py
@@ -1,11 +1,19 @@
 import pytest
 import numpy as np
+from numpy import allclose
+
 from compute import divide
 from compute import multiply
 
 def test_divide():
     x = divide(1,2)
     assert x == pytest.approx(0.5)
+
+def test_divide_array():
+    a = np.array([1,1,1])
+    b = np.array([2,2,2])
+    x = divide(a,b)
+    assert_allclose(x, np.array([0.5,0.5,0.5]))
 
 def test_divide_zero():
     assert np.isinf(divide(1,0))
