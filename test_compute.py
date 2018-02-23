a09c2947b8f4a5f1a585871de9a5a7edded425a5
marcioabcr <32570044+marcioabcr@users.noreply.github.com>
2018-02-23 11:46:30 +0100

    Test

--- test_compute.py
+++ test_compute.py
@@ -1,10 +1,14 @@
 import pytest
+import numpy as np
 from compute import divide
 from compute import multiply
 
 def test_divide():
     x = divide(1,2)
     assert x == pytest.approx(0.5)
+
+def test_divide_zero():
+    assert np.isinf(divide(1,0))
 
 def test_multiply():
     assert multiply(2,2) == 4
